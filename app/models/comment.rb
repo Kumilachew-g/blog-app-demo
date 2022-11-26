c826d4cdf36d25b8b0621c220bc3b13d3bb5ccd9
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-26 18:32:07 +0300

    Implement update and size method in update_comments_count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,11 @@ class Comment < ApplicationRecord
     blongs_to :post
     belongs_to :author, class_name: "User", foreign_key: :user_id
 
-    def update_comments_count    
+    after_save :update_comments_count
+
+    private
+    
+    def update_comments_count
+        post.update(comments_count: post.comments.size)    
     end
 end
