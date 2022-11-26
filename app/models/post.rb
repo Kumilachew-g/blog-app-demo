027cab828be2d41d900c152daac9746155f65fe2
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-26 18:20:58 +0300

    Create most_recent_five_comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
     has_many :comments
     has_many :likes
     belongs_to :author, class_name: 'User', foreign_key: :user_id
+
+    def most_recent_five_comments  
+    end
 end
