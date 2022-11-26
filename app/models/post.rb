e4caf62f0aafe45c862a8631c6fef2c638bce577
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-26 18:26:49 +0300

    Add after_save association in post class model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
     has_many :likes
     belongs_to :author, class_name: 'User', foreign_key: :user_id
 
+    after_save :update_posts_count
+
     def most_recent_five_comments
         comments.order(created_at: :desc).limit(5)  
     end
