411852b9a26a73f87485e7ccf1d9f1af3c3553f5
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-26 18:21:44 +0300

    Implement order and limit methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
     has_many :likes
     belongs_to :author, class_name: 'User', foreign_key: :user_id
 
-    def most_recent_five_comments  
+    def most_recent_five_comments
+        comments.order(created_at: :desc).limit(5)  
     end
 end
