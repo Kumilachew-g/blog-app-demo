e48720c3762de544b71bc6b1d2b9eab736a0ab4a
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-26 18:29:42 +0300

    Add after_save association

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,10 @@ class Like < ApplicationRecord
     belongs_to :post
     belongs_to :author, class_name: 'User', foreign_key: :user_id
 
+    after_save :update_likes_count
+
+    private
+
     def update_likes_count
         post.update(likes_count: post.likes.size)   
     end
