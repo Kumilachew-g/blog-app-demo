1408a2d663690a335af60e1aebe83e8d141b9717
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-26 18:29:03 +0300

    Implement update and size methods in like class model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,7 @@ class Like < ApplicationRecord
     belongs_to :post
     belongs_to :author, class_name: 'User', foreign_key: :user_id
 
-    def update_likes_count   
+    def update_likes_count
+        post.update(likes_count: post.likes.size)   
     end
 end
