d2cd5d257514a33bd538a0ae17633f72f6b31352
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-27 21:20:20 +0300

    Add test for comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Comment model' do
+    subject { Comment.new(text: 'text test', user_id: 1, post_id: 1) }
+
+    before { subject.save }
+
+    it 'check if it increases the comments' do
+      old_comments_counter = Post.find(1).comments_counter
+      subject.update_comments_counter
+      expect(Post.find(1).comments_counter).to eq(old_comments_counter + 1)
+    end
+  end
 end
