a3562f5202b6edb330bdcade530673043b020a97
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-27 21:35:19 +0300

    Fix missing letters

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   belongs_to :author, class_name: 'User', foreign_key: :user_id
 
-  after_save :update_posts_count
+  after_save :update_posts_counter
 
   validates :title, presence: true, length: {maximum: 250}
   validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
